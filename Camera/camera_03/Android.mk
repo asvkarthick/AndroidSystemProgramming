6c380d836f81163839c42bea2b5688b63f71f17f
Karthick Kumaran <asvkarthick@gmail.com>
2018-05-10 15:53:12 -0700

    Added vendor tag and moved init()

---
+++ Camera/camera_03/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := camera_03.cpp
+
+LOCAL_CFLAGS := -Wall -Wextra -Werror
+LOCAL_CFLAGS += -DFDLEAK_FLAG
+LOCAL_CFLAGS += -DMEMLEAK_FLAG
+
+LOCAL_CFLAGS += -std=c++11 -std=gnu++1y
+
+LOCAL_C_INCLUDES := system/media/camera/include \
+                    hardware/libhardware/include/hardware
+
+LOCAL_SHARED_LIBRARIES := libhardware
+
+LOCAL_MODULE := camera_03
+
+LOCAL_32_BIT_ONLY := true
+include $(BUILD_EXECUTABLE)
